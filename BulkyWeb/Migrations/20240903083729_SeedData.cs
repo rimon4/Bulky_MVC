c58cbe2bdb6ef6fad4370e5cf34555c98a0446d9
nmrose <nowshinmeheta@gmail.com>
2024-09-03 21:26:00 +0600

    Migrations

---
+++ BulkyWeb/Migrations/20240903083729_SeedData.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BulkyWeb.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "categories",
+                columns: new[] { "Id", "CategoryName", "DisplayOrder" },
+                values: new object[] { 4, "Sifi", 4 });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "categories",
+                keyColumn: "Id",
+                keyValue: 4);
+        }
+    }
+}
