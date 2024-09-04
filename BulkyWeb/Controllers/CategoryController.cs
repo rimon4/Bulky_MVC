e2b104adfd6c53cf0fa21bace4ff6dce43447b69
RimonSaha <18101047@uap-bd.edu>
2024-09-04 15:41:26 +0600

    Retrieving a category's details for editing, fetching data based on category ID and handling form submissions and data updates in the database.

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -33,5 +33,30 @@ namespace BulkyWeb.Controllers
             return View();
         }
 
+        public IActionResult Edit(int id)
+        {
+            if(id==null || id==0)
+            {
+                return NotFound();
+            }
+            Category categoryFromDb = _db.categories.Find(id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
     }
 }
